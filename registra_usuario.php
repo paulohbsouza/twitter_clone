853637db7a2e9bdfd99559aa344be5cf597ff50b
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-01-19 18:03:45 -0300

    Update registra_usuario.php

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,7 +1,21 @@
 <?php 
 
-	$_POST['usuario'];
-	$_POST['email'];
-	$_POST['senha'];
+	require_once('db.class.php');
+
+	$usuario = $_POST['usuario'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
+
+	$objDb = new db();
+	$link = $objDb->conecta_mysql();
+
+	$sql = "insert into usuarios(usuario, email, senha) values('$usuario','$email','$senha')";
+
+	//executar a query
+	if(mysqli_query($link, $sql)){
+		echo "Usuário regsitrado com sucesso!";
+	} else {
+		echo "Erro ao registrar o usuário";
+	}
 
 ?>
