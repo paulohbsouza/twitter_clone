0a89835377c25e9f82177f0b90de4cad9fb67240
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-01-20 17:47:52 -0300

    Update validar_acesso.php

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,9 +1,11 @@
 <?php
 
+	session_start();
+
 	require_once('db.class.php');
 
 	$usuario = $_POST['usuario'];
-	$senha = $_POST['senha'];
+	$senha = md5($_POST['senha']);
 
 	$sql = " SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
 
@@ -14,7 +16,16 @@
 
 	if ($resultado_id) {
 		$dados_usuario = mysqli_fetch_array($resultado_id);
-		var_dump($dados_usuario);
+		
+		if(isset($dados_usuario['usuario'])){
+
+			$_SESSION['usuario'] = $dados_usuario['usuario'];
+			$_SESSION['email'] = $dados_usuario['email'];
+
+			header('Location: home.php');
+		} else {
+			header('Location: index.php?erro=1');
+		}
 	
 	} else {
 		echo "Erro na execução da consulta.";
