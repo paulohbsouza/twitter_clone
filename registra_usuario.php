b72594e138c323abbfaafcb33d6da95df5d6a42b
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-01-22 11:27:12 -0300

    Update registra_usuario.php

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,6 +9,47 @@
 	$objDb = new db();
 	$link = $objDb->conecta_mysql();
 
+	$usuario_existe = false;
+	$email_existe = false;
+
+	//verificar se o usuario ja existe
+	$sql = "select * from usuarios where usuario = '$usuario' ";
+	if ($resultado_id = mysqli_query($link, $sql)){
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		if (isset($dados_usuario['usuario'])){
+			$usuario_existe = true;
+		} 
+	} else {
+		echo "erro ao tentar cadastrar usuario";
+	}
+
+	// verificar se o email ja existe
+	$sql = "select * from usuarios where email = '$email' ";
+	if ($resultado_id = mysqli_query($link, $sql)){
+		$dados_usuario = mysqli_fetch_array($resultado_id);
+		if (isset($dados_usuario['email'])){
+			$email_existe = true;
+		}
+	} else {
+		echo "erro ao tentar cadastrar email";
+	}
+
+	if ($usuario_existe || $email_existe) {
+
+		$retorno_get = '';
+
+		if ($usuario_existe) {
+			$retorno_get.= "erro_usuario=1&";
+		}
+
+		if ($email_existe) {
+			$retorno_get.= "erro_email=1&";
+		}
+
+		header('Location: inscrevase.php?'.$retorno_get);
+		die();
+	}
+
 	$sql = "insert into usuarios(usuario, email, senha) values('$usuario','$email','$senha')";
 
 	//executar a query
