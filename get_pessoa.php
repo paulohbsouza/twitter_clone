4713941da69415c87413b0b8fa4b744e0d3d240b
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-01-26 18:19:03 -0300

    Create get_pessoa.php

---
+++ get_pessoa.php
@@ -0,0 +1,34 @@
+<?php
+
+	session_start();
+
+	if(!isset($_SESSION['usuario'])){
+		header('Location: index.php?erro=1');
+	}
+
+	require_once('db.class.php');
+
+	$nome_pessoa = $_POST['nome_pessoa'];
+	$id_usuario = $_SESSION['id_usuario'];
+
+	$objDb = new db();
+	$link = $objDb->conecta_mysql();
+	
+	$sql = " SELECT * FROM usuarios WHERE usuario like '%$nome_pessoa%' AND id <> '$id_usuario' ";
+
+	//echo $sql;
+
+	$resultado_id = mysqli_query($link, $sql);
+
+	if ($resultado_id) {
+		while ($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)) {
+			echo ' <a href="#" class="list-group-item"> ';
+				echo '<strong>'.$registro['usuario'].'</strong> - <small>'.$registro['email'].'</small>';
+			echo ' </a>';
+		}
+	} else {
+		echo "erro na consulta de usuários no banco de dados!";
+	}
+
+
+?>
