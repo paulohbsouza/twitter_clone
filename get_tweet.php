3a5d0b9ee406684e3cda01c4c9fc54dc6df5034a
Paulo Henrique Bernardes <42076064+paulohbsouza@users.noreply.github.com>
2021-01-26 18:19:15 -0300

    Update get_tweet.php

--- get_tweet.php
+++ get_tweet.php
@@ -8,15 +8,29 @@
 
 	require_once('db.class.php');
 
-	$id_usuario = $_SESSION['id'];
+	$id_usuario = $_SESSION['id_usuario'];
 
 	$objDb = new db();
 	$link = $objDb->conecta_mysql();
 	
-	$sql = " SELECT * FROM tweet WHERE id_usuario = $id_usuario ORDER BY data_inclusao DESC ";
+	$sql = " SELECT DATE_FORMAT(t.data_inclusao, '%d %b %Y - %T') AS data_inclusao_formatada, t.data_inclusao, t.tweet, u.usuario ";
+	$sql.= " FROM tweet AS t JOIN usuarios AS u ON (t.id_usuario = u.id) ";
+	$sql.= " WHERE id_usuario = $id_usuario ORDER BY data_inclusao DESC ";
 
-	echo $sql;
+	//echo $sql;
 
-	//mysqli_query($link, $sql);
+	$resultado_id = mysqli_query($link, $sql);
+
+	if ($resultado_id) {
+		while ($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)) {
+			echo ' <a href="#" class="list-group-item"> ';
+				echo '<h4 class="list-group-item-heading">'.$registro['usuario'].' <small>- '.$registro['data_inclusao_formatada'].'</small></h4>';
+				echo '<p class="list-group-item-text">'.$registro['tweet'].'</p>';
+			echo ' </a>';
+		}
+	} else {
+		echo "erro na consulta de tweets no banco de dados!";
+	}
+
 
 ?>
